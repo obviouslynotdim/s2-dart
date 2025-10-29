7bd8361fad08ed6480b864f89a03b634bec8f049
Setha Vathanak <154043526+obviouslynotdim@users.noreply.github.com>
2025-10-29 14:58:21 +0700

    Update ex2.dart

--- w4_manipulatewidget/ex2.dart
+++ w4_manipulatewidget/ex2.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Container(
+        margin: EdgeInsets.all(50),
+        padding: EdgeInsets.all(40),
+        color: Colors.blue[300],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue[600],
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Center(
+            child: Text(
+              "CADT STUDENTS",
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
