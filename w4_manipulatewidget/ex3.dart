409793b3eb176a103a5fa0bdb0a0ec6adeb87115
Setha Vathanak <154043526+obviouslynotdim@users.noreply.github.com>
2025-10-29 14:58:47 +0700

    Update ex3.dart

--- w4_manipulatewidget/ex3.dart
+++ w4_manipulatewidget/ex3.dart
@@ -1,1 +1,62 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[350],
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30), // 👈 Added padding here
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(5),
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  child: const Text(
+                    "OOP",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(45),
+                    color: Colors.blue[100],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(5),
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  child: const Text(
+                    "DART",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(45),
+                    color: Colors.blue[300],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(5),
+                  margin: const EdgeInsets.symmetric(vertical: 20),
+                  child: const Text(
+                    "FLUTTER",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(45),
+                    color: Colors.blue[600],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
