6484d595b82db5e194269b0fecab769a1af3957c
Setha Vathanak <154043526+obviouslynotdim@users.noreply.github.com>
2025-11-09 17:54:46 +0700

    Delete t14-project2/hospitalManagement directory

--- t14-project2/hospitalManagement/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hospital_management_system/data/hospital_repo.dart';
-import 'package:hospital_management_system/ui/console_ui.dart';
-
-void main() {
-  final repo = HospitalRepository();
-  final ui = ConsoleUI(repo);
-  ui.start();
-}
